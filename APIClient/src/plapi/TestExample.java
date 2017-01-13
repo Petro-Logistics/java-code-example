99242de394e63bba56f126dc75f6acfa562b612a
User <info@canterburycomputers.com>
2017-01-13 05:17:15 +0100

    update test api credentials

--- APIClient/src/plapi/TestExample.java
+++ APIClient/src/plapi/TestExample.java
@@ -12,12 +12,12 @@ public class TestExample {
 			System.out.println("Creating a PLAPIClient object...");
 			
 			/* Initializing a new PLAPIClient object */
-			PLAPIClient client = new PLAPIClient("https://secure.petro-logistics.com/api/v2/movementsdata", "stacey_http_7zz7lQ28eWFK", "6AsLJVuKpx4miX47151JK82BvMXKNR2S", "o79z9ff1or702t09tr6d48xs", "qEbl7HsOasyzLoqrdLZF6Na9aqd9xJ2n7Zkdz92jekk7LknSxD3QAfguLnWZjpFU");
+			PLAPIClient client = new PLAPIClient("https://secure.petro-logistics.com/api/v2/aggregatemovementsdata", "testuser_http_CuH68Omfx17R", "X9PV5EmJPr88lEyjD2I2IE26b9ElQCX0", "37rspm6j39td23nh0o2v0h78", "P0iwW39qaMvTjFRdcmsiKmD9OxGEquHNXapwbSQr8gbuV2ssqjbt0Vy7Yelyi4C1");
 			
 			System.out.println("Executing the query...");
 			
 			/* Executing the request and storing the result in a variable */
-			JSONObject obj = client.execute("Nigeria_Oct_2016_p30");
+			JSONObject obj = client.execute("Angola_Test_Data");
 			
 			/* Print out the result if the request was successful otherwise print "FAIL" */
 			System.out.println((obj == null) ? "FAIL" : obj.toString(3));
